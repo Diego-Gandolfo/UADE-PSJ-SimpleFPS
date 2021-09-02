7fc257489147c67c7a79e783343ba02748ea5b31
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-09-01 22:11:43 -0300

    Agregué el Pool de objetos

---
+++ Assets/_Main/Scripts/Generics/Pool.cs
@@ -0,0 +1,53 @@
+﻿using Assets._Main.Scripts.Strategy;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Main.Scripts.Generics
+{
+    public class Pool<T> : IPool<T> where T : MonoBehaviour
+    {
+        private List<T> inUse = new List<T>();
+        private List<T> available = new List<T>();
+
+
+        public Pool(List<T> values)
+        {
+            foreach (var item in values)
+            {
+                Store(item);
+            }
+        }
+
+        public T GetInstance()
+        {
+            if(IsAvailable() > 0)
+            {
+                T temp = available[0];
+                available.Remove(temp);
+                inUse.Add(temp);
+                temp.gameObject.SetActive(true);
+                return temp;
+            }
+            return default(T);
+        }
+
+        public int IsAvailable()
+        {
+            return available.Count;
+        }
+
+        public void Store(T item)
+        {
+            available.Add(item);
+            item.gameObject.SetActive(false);
+            if (inUse.Contains(item)) //Si esta en la lista...
+                inUse.Remove(item); //Removelo
+        }
+        public List<T> GetInUseItems()
+        {
+            List<T> list = new List<T>();
+            list.AddRange(inUse);
+            return list;
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/Strategy/IPool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._Main.Scripts.Strategy
+{
+    interface IPool<T>
+    {
+        void Store(T item);
+        T GetInstance();
+        int IsAvailable();
+    }
+}
