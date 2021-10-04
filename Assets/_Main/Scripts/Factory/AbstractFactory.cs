ca53b2f041eec85deaad09adaf501b3b81939712
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-10-04 16:49:28 -0300

    Update AbstractFactory.cs

--- Assets/_Main/Scripts/Factory/AbstractFactory.cs
+++ Assets/_Main/Scripts/Factory/AbstractFactory.cs
@@ -5,10 +5,20 @@ namespace SimpleFPS.Factory
 {
     public abstract class AbstractFactory<T> where T : MonoBehaviour
     {
+        #region Protected Fields
+
         protected T _prefab;
         protected Pool<T> _pool;
 
+        #endregion
+
+        #region Propertys
+
         public Pool<T> Pool => _pool;
+
+        #endregion
+
+        #region Constructor
 
         public AbstractFactory(T prefab)
         {
@@ -16,9 +26,15 @@ namespace SimpleFPS.Factory
             InitializePool();
         }
 
+        #endregion
+
+        #region Private Methods
+
         private void InitializePool()
         {
             _pool = new Pool<T>(_prefab);
         }
+
+        #endregion
     }
 }
